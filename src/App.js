baf8cfaf1108657c2e1a035d2ec17cbb25f584a9
DaEunKwon0109 <113701669+DaEunKwon0109@users.noreply.github.com>
2024-03-26 14:34:53 +0900

    update code

--- src/App.js
+++ src/App.js
@@ -26,44 +26,52 @@ function App() {
         canvasCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
         canvasCtx.drawImage(results.image, 0, 0, canvasRef.current.width, canvasRef.current.height);
 
-        drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {color: '#00FF00', lineWidth: 1});
-        drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, {color: '#FF0000', lineWidth: 0.5});
+        drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {color: '#00FF00', lineWidth: 1.5});
+        drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, {color: '#FFFFFF', lineWidth: 0.5});
 
-        const rightShoulder = results.poseLandmarks && results.poseLandmarks[12];
         const leftShoulder = results.poseLandmarks && results.poseLandmarks[11];
+        const rightShoulder = results.poseLandmarks && results.poseLandmarks[12];
 
         if (rightShoulder && leftShoulder) {
-            const shoulderMidPoint = (rightShoulder + leftShoulder) / 2;
+
+            const shoulderMidPointZ = (rightShoulder.z + leftShoulder.z) / 2;
+
+            const shoulderMidPointX = (rightShoulder.x + leftShoulder.x) / 2;
+            const shoulderMidPointY = (rightShoulder.y + leftShoulder.y) / 2;
+
             const noseLandmark = results.faceLandmarks && results.faceLandmarks[0];
             const chinLandmark = results.faceLandmarks && results.faceLandmarks[152];
 
-            // z값을 이용해서 거북목 자세인지 판단//코 어깨중심
-            // const Zvalues= checkZValues(noseLandmark, shoulderMidPoint);
-            //턱끝과 어깨 중심 사이 거리
-            const distance = checkDistance(chinLandmark, shoulderMidPoint);
-            //턱끝과 어깨 중심의 2차원 각도 계산
-           // const angle = checkAngle(chinLandmark, shoulderMidPoint);
+            // z값을 이용해서 거북목 자세인지 판단 -> 코와 어깨중심 - 해리
+            const Zvalues= checkZValues(noseLandmark, shoulderMidPointZ); 
+
+            //턱끝과 어깨 중심 사이 거리(distance) - 다은
+            const distance = checkDistance(chinLandmark, shoulderMidPointX, shoulderMidPointY);
+            //턱끝과 어깨 중심의 2차원 각도 계산(angle) - 다은
+            const angle = checkAngle(chinLandmark, leftShoulder, shoulderMidPointX, shoulderMidPointY);
 
 
-            if(noseLandmark){
-              //if(Zvalues||distance||angle){
-              if(distance){
-                canvasCtx.font = "30px Arial";
+            if(noseLandmark){ // 0번 랜드마크가 인식될 경우 
+              if(Zvalues || distance || angle){
+                canvasCtx.font = "10px Arial";
                 canvasCtx.fillStyle = "red";
-                canvasCtx.fillText("올바르지 않은 자세입니다.", 10, 50);
+                canvasCtx.fillText("You have to fix your pose.", 10, 30);
               }else {
-                canvasCtx.font = "30px Arial";
+                canvasCtx.font = "10px Arial";
                 canvasCtx.fillStyle = "green";
-                canvasCtx.fillText("정상 자세입니다.", 10, 50);
+                canvasCtx.fillText("Your pose is normal", 10, 30);
               }
-            }else{
-              canvasCtx.font = "30px Arial";
+            }else{ // 0번 랜드마크 인식이 안될 경우 -> 예외 처리
+              canvasCtx.font = "10px Arial";
                 canvasCtx.fillStyle = "red";
-                canvasCtx.fillText("인식이 제대로 되지 않는군요.", 10, 50);
+                canvasCtx.fillText("No recognized...", 10, 30);
             }
-
+            
+            // Diaplay angle and distance in camera window 
+            // 두 함수가 True or False만 리턴하기 때문에 값을 직접적으로 받아올 수 없음.???
+            // canvasCtx.fillText(`Angle: ${...} degrees`);
+            // canvasCtx.fillText(`Distance: ${...} pixels`);
         }
-
         canvasCtx.restore();
       });
 
